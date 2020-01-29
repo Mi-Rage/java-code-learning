d7e905150cd33dc851425bb94b399f65ae9dcbcf
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-01-29 20:03:30 +0300

    Encryptor.java
    Новая версия Encryptor, теперь с поддержкой файлов и обработкой исключений.
    Функционал тот же, в командной строке передаем ключи шифровать или расшифровать, ключ шифра и текстовую строку для работы или имя файла с исходным текстом и имя файла для записи результата.
    
    Полное описание здесь:
    Description
    At this stage, you need to add the ability to read and write original and cipher data to files. The program must parse two additional arguments -in and -out to specify the full name of a file to read data and to write the result. Arguments -mode, -key, and -data should still work as before.
    
    Your program should read data from -data or from a file written in the -in argument. That's why you can't have both -data and -in arguments simultaneously, only one of them.
    
    If there is no -mode, the program should work in enc mode.
    If there is no -key, the program should consider that key = 0.
    If there is no -data, and there is no -in the program should assume that the data is an empty string.
    If there is no -out argument, the program must print data to the standard output.
    If there are both -data and -in arguments, your program should prefer -data over -in.
    If there is a non-standard situation (an input file does not exist or an argument doesn’t have a value), the program should not fail. Instead, it must display a clear message about the problem and stop successfully. The message should contain the word "Error" in any case.

--- Encryptor.java
+++ Encryptor.java
@@ -1,9 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 public class Encryptor {
     public static void main(String[] args) {
 
         String mode = "enc"; // По умолчанию mode в режиме enc. Даже если параметр не передан.
         int shift = 0;       // По умолчанию ключ сдвига ноль, даже если параметр не передан.
         String buf = "";     // По умолчанию строка шифрования пустая, даже если параметр не передан.
+        String filePathIn = "";// По умолчанию путь к файлу чтения пустой.
+        String filePathOut = "";// По умолчанию путь к файлу записи пустой.
+
+        if(args.length == 0){
+            System.out.println("Error : Args is NULL");
+        }
 
         for(int i = 0; i < args.length; i++){
             if(args[i].equals("-mode")){
@@ -13,57 +26,119 @@ public class Encryptor {
                 shift = Integer.parseInt(args[i + 1]);
             }
             if(args[i].equals("-data")){
-                buf = buf + args[i + 1];
+                buf = args[i + 1];
+            }
+            if(args[i].equals("-in")){
+                filePathIn = args[i + 1];
+            }
+            if(args[i].equals("-out")){
+                filePathOut = args[i + 1];
             }
         }
-        // Полученный аргумент переводим в массив символов
+
+
+        // Полученный аргумент -data переводим в массив символов
         char[] chars = buf.toCharArray();
+
+        boolean isDataAndIn = (!buf.equals("") && !filePathIn.equals("")) || (!buf.equals("") && filePathIn.equals(""));
+        boolean isNoDataYesIn = (buf.equals("") && !filePathIn.equals(""));
+        boolean isNoFileOut = (!filePathOut.equals(""));
 
         switch (mode) {
             case "enc":
-                encrypt(chars, shift);
+                if(isDataAndIn){                                    // Если есть и -data и -in
+                    System.out.println(encrypt(chars, shift));      // в приоритете -data
+                    break;
+                } else if(isNoDataYesIn){                           // Нет -data есть -in
+                    try {
+                        buf = readInFile(filePathIn);               // Читаем файл
+                    } catch (IOException e) {
+                        System.out.println("Error : Cannot read file: " + e.getMessage());
+                    }
+                    char[] charsFileEnc = buf.toCharArray();
+                    if(isNoFileOut){
+                        writeOutFile(filePathOut, encrypt(charsFileEnc, shift)); // Если есть -out то пишем в файл
+                    } else {
+                        System.out.println(encrypt(charsFileEnc, shift)); // Если нет -out то выводим в консоль
+                    }
+                }
                 break;
+
             case "dec":
-                decrypt(chars, shift);
+                if(isDataAndIn){                                    // Если есть и -data и -in
+                    System.out.println(decrypt(chars, shift));      // в приоритете -data
+                    break;
+                } else if(isNoDataYesIn){                           // Нет -data есть -in
+                    try {
+                        buf = readInFile(filePathIn);               // Читаем файл
+                    } catch (IOException e) {
+                        System.out.println("Error : Cannot read file: " + e.getMessage());
+                    }
+                    char[] charsFileDec = buf.toCharArray();
+                    if(isNoFileOut){
+                        writeOutFile(filePathOut, decrypt(charsFileDec, shift)); // Если есть -out то пишем в файл
+                    } else {
+                        System.out.println(decrypt(charsFileDec, shift)); // Если нет -out то выводим в консоль
+                    }
+                }
                 break;
             default:
-                System.out.println("Unknown operation");
+                System.out.println("Error : Unknown operation");
                 break;
         }
     }
 
     // Шифруем
-    public static void encrypt(char[] chars, int shift) {
+    private static String encrypt(char[] chars, int shift) {
+        StringBuilder encString = new StringBuilder(); // Защифрованная строка по умолчанию
         char start = 32;    // Первый символ ASCII
         char finish = 127;  // Последний символ ASCII
         int size = 96;      // К-во символов в таблице ASCII
 
-        for (int i = 0, charsLength = chars.length; i < charsLength; i++) {
-            char item = chars[i];
+        for (char item : chars) {
             if (item >= start && item <= finish) {
                 char shiftItem = (char) (((item - start + shift) % size) + start);
-                System.out.print(shiftItem);
+                encString.append(shiftItem);
             } else {
-                System.out.print(item);
+                encString.append(item);
             }
         }
+        return encString.toString(); // Возвращаем зашифрованную строку
     }
 
     // Расшифровываем
-    private static void decrypt(char[] chars, int shift) {
+    private static String decrypt(char[] chars, int shift) {
+        StringBuilder decString = new StringBuilder(); // Расшифрованная строка по умолчанию
         char start = 32;        // Первый символ ASCII
         char finish = 127;      // Последний символ ASCII
         int size = 96;          // К-во символов в таблице ASCII
 
-        for (int i = 0, charsLength = chars.length; i < charsLength; i++) {
-            char item = chars[i];
+        for (char item : chars) {
             if (item >= start && item <= finish) {
                 char shiftItem = (char) (finish - (finish - item + shift) % size);
-                System.out.print(shiftItem);
+                decString.append(shiftItem);
             } else {
-                System.out.print(item);
+                decString.append(item);
             }
         }
+        return decString.toString(); // Возвращаем расшифрованную строку
     }
+
+    // Читаем файл
+    public static String readInFile(String filePathIn) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePathIn)));
+    }
+
+    // Пишем в файл
+    public static void writeOutFile(String filePathOut, String decString){
+        File file = new File(filePathOut);
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            printWriter.println(decString); // Пишем в файл полученную зашифрованную строку
+        } catch (IOException e) {
+            System.out.printf("Error : An exception occurs %s", e.getMessage());
+        }
+    }
+
+
 
 }
